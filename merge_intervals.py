13410434d01d54fe92b44c0699393e90fa4f4425
Diana Okrut <zyndel94@gmail.com>
2020-07-30 21:46:54 +0300

    def merge()

--- merge_intervals.py
+++ merge_intervals.py
@@ -6,15 +6,32 @@ def merge(intervals):
     [[1, 5]]
     >>> merge([])
     []
+    >>> merge([[1, 4], [0, 4]])
+    [[0, 4]]
+    >>> merge([[4, 5], [6, 7], [1, 4]])
+    [[1, 5], [6, 7]]
+    >>> merge([[4, 5], [1, 10], [2, 4]])
+    [[1, 10]]
+    >>> merge([[1, 4],[5, 6]])
+    [[1, 4], [5, 6]]
+    >>> merge([[1, 4], [2, 3]])
+    [[1, 4]]
     """
     if not intervals:
         return intervals
+
+    if len(intervals) == 1:
+        return intervals
+
+    intervals.sort()
     new = []
     comp = intervals[0]
     for el in intervals[1:]:
-        if comp[1] >= el[0]:
+        if el[1] >= comp[1] >= el[0]:
             comp[1] = el[1]
             new.append(comp)
+        elif el[1] <= comp[1] >= el[0]:
+            continue
         else:
             new.append(el)
             comp = el
