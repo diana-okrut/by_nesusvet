085912cb442cc3eb8b58e5dd4e7cdbbae5b3e765
Diana Okrut <zyndel94@gmail.com>
2020-08-17 09:15:23 +0300

    new func main_2

--- medium-chain_of_monsters.py
+++ medium-chain_of_monsters.py
@@ -9,7 +9,7 @@ def main(monsters):
     """
 
     start_index = monsters.index(min(monsters))
-    new_monsters_list = monsters[start_index::] + monsters[:start_index]
+    new_monsters_list = monsters[start_index:] + monsters[:start_index]
 
     result = 0
     damage = 0
@@ -20,6 +20,29 @@ def main(monsters):
         damage = dam
 
     return result
+
+
+def main_2(monsters):
+    """
+    >>> main_2([(7, 15), (2, 14), (5, 3)])
+    6
+    >>> main_2([(9, 44), (18, 3), (5, 2), (1, 3), (5, 7), (9, 11)])
+    7
+    """
+    start_index = monsters.index(min(monsters))
+    new_monsters_list = monsters[start_index:] + monsters[:start_index]
+
+    damage = 0
+    alive_monsters = []
+
+    for monster, dam in new_monsters_list:
+        monster -= damage
+        alive_monsters.append(monster)
+        damage = dam
+
+    result = [mon for mon in alive_monsters if mon > 0]
+
+    return sum(result)
 
 
 if __name__ == '__main__':
